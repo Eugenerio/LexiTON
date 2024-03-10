46505959617e404f1016eab9eef63af5f808beb2
jusikXL <125464973+jusikXL@users.noreply.github.com>
2024-03-10 14:31:52 +0100

    init

---
+++ src/app/config/tma.ts
@@ -0,0 +1,7 @@
+import { type SDKInitOptions } from '@tma.js/sdk-react';
+
+export const options: SDKInitOptions = {
+  acceptCustomStyles: true,
+  cssVars: true,
+  async: true,
+};
---
+++ src/app/providers.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { SDKProvider, DisplayGate, useMiniApp } from '@tma.js/sdk-react';
+import { useEffect } from 'react';
+import { options } from './config/tma';
+
+function SDKProviderError({ error }: { error: unknown }) {
+  return (
+    <div>
+      Oops. Something went wrong.
+      <blockquote>
+        <code>{error instanceof Error ? error.message : JSON.stringify(error)}</code>
+      </blockquote>
+    </div>
+  );
+}
+
+function SDKProviderLoading() {
+  return <div>SDK is loading.</div>;
+}
+
+function SDKInitialState() {
+  return <div>Waiting for initialization to start.</div>;
+}
+
+export default function Providers({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <SDKProvider options={options}>
+      <DisplayGate error={SDKProviderError} loading={SDKProviderLoading} initial={SDKInitialState}>
+        {children}
+      </DisplayGate>
+    </SDKProvider>
+  );
+}
