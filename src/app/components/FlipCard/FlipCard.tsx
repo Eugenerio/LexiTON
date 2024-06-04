7ca4625a37c18b53b18d260cfb2ec20f2cd41fcf
Raman Kulpeksha <roman.kulpekshark@gmail.com>
2024-06-04 20:14:29 +0200

    feat(flipcard): adding proper styles

--- src/app/components/FlipCard/FlipCard.tsx
+++ src/app/components/FlipCard/FlipCard.tsx
@@ -14,16 +14,16 @@ function FlipCard() {
 
     return (
         <div
-            className={`relative w-full h-60 select-none ${styles.transformStylePreserve3d} transition-transform duration-700 ${isFlipped ? styles.rotateY180 : ''}`}
+            className={`relative w-full h-60 select-none ${styles.transformStylePreserve3d} bg-black rounded-3xl text-white transition-transform duration-700 ${isFlipped ? styles.rotateY180 : ''}`}
             onClick={handleFlip}
         >
             {/* Front Side */}
-            <div className={`absolute w-full h-full bg-white rounded-lg shadow-lg flex justify-center items-center ${styles.backfaceHidden}`}>
+            <div className={`absolute w-full h-full ${styles.background} rounded-lg shadow-lg rounded-3xl flex justify-center items-center ${styles.backfaceHidden}`}>
                 <h3 className="text-2xl font-bold">{title}</h3>
             </div>
 
             {/* Back Side */}
-            <div className={`absolute w-full h-full bg-gray-100 rounded-lg shadow-lg flex justify-center items-center ${styles.backfaceHidden} ${styles.rotateY180}`}>
+            <div className={`absolute w-full h-full rounded-lg shadow-lg rounded-3xl flex justify-center text-center items-center ${styles.backfaceHidden} ${styles.rotateY180}`}>
                 <p className="p-4 text-lg">{description}</p>
             </div>
         </div>
