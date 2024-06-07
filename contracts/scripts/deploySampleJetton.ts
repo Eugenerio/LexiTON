e1e9ce894335370dbc8c160703025831f004ba65
yriaboki <01184464@pw.edu.pl>
2024-06-07 19:23:36 +0200

    feat(contracts): create jetton contract

---
+++ contracts/scripts/deploySampleJetton.ts
@@ -0,0 +1,36 @@
+import { Address, toNano } from '@ton/core';
+import { SampleJetton } from '../wrappers/SampleJetton';
+import { NetworkProvider } from '@ton/blueprint';
+import { buildOnchainMetadata } from '../utils/jetton-helpers';
+
+export async function run(provider: NetworkProvider) {
+    const jettonParams = {
+        name: 'LEXI coin',
+        description: 'Official token of LexiTon project',
+        symbol: 'LEXI',
+        image: 'https://magenta-useful-catshark-496.mypinata.cloud/ipfs/QmWUD2g48SDqkjSoowQn4e4LFnrhoak6kuMgmPvzK8ZMKh',
+    };
+
+    // Create content Cell
+    let content = buildOnchainMetadata(jettonParams);
+
+    const sampleJetton = provider.open(
+        await SampleJetton.fromInit(provider.sender().address as Address, content, 1000000000000000000n),
+    );
+
+    await sampleJetton.send(
+        provider.sender(),
+        {
+            value: toNano('0.05'),
+        },
+        {
+            $$type: 'Mint',
+            amount: 1000n,
+            receiver: provider.sender().address as Address,
+        },
+    );
+
+    await provider.waitForDeploy(sampleJetton.address);
+
+    // run methods on `sampleJetton`
+}
