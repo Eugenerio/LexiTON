e9284ac8e9e1075e5b2fc5bfe3801c019b659736
Raman Kulpeksha <roman.kulpekshark@gmail.com>
2024-06-09 11:40:01 +0200

    feat(logo): updating logo

--- src/app/components/Loader/Loader.tsx
+++ src/app/components/Loader/Loader.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from './Loader.module.css';
+import Image from 'next/image'
+import logo from '@/app/images/logo.png'
 
 function Loader() {
   return (
     <div
       className={`fixed left-0 top-0 flex h-full w-full items-center justify-center bg-cover bg-center bg-no-repeat ${styles.background} ${styles.appear}`}
     >
-      <h1 className={`text-5xl font-bold text-white ${styles.appear}`}>LexiTON</h1>
+      <Image src={logo} width={245} height={103} className={` ${styles.appear}`} alt='LexiTON logo' priority={true} />
     </div>
   );
 }
